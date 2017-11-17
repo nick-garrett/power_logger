7ea834732ec228999c6a6f0d94d73c34ff9a135f
Nick Garrett <nick.garrett@fluxfederation.com>
2017-11-17 14:44:08 +1300

    Made it look slightly nicer

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,11 @@
 class Device < ApplicationRecord
   has_many :readings
+
+  def state_as_word
+    state ? "on" : "off"
+  end
+
+  def switch_state_word
+    state ? "Turn off" : "Turn on"
+  end
 end
